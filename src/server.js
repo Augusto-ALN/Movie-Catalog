06e88d3788ae80e25a2426cfc9b7613570b72dae
Augusto Alves de Lima Neto <augustoaln20@gmail.com>
2021-10-05 20:32:48 -0300

    feat: change route name

--- src/server.js
+++ src/server.js
@@ -6,7 +6,7 @@ const axios = require('axios')
 
 app.use(cors())
 
-app.get('/', async(req, res) => {
+app.get('/popular', async(req, res) => {
     try {
         const { data } = await axios(`https://api.themoviedb.org/3/discover/movie?api_key=${process.env.API_KEY}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_watch_monetization_types=flatrate`)
         return res.json(data)
