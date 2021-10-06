5420923866daf3be270bebf67b80a76dfff6ac59
Augusto Alves de Lima Neto <augustoaln20@gmail.com>
2021-10-06 00:38:35 -0300

    feat: filter for findPopularMovies results

--- src/utils/findPopularMovies.js
+++ src/utils/findPopularMovies.js
@@ -2,5 +2,13 @@ const axios = require('axios').default
 
 module.exports.findPopularMovies = async () => {
     const {data} = await axios.get(`https://api.themoviedb.org/3/discover/movie?api_key=${process.env.API_KEY}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_watch_monetization_types=flatrate`)
-    return data
+    const catalog = data.results.map(movie => {
+        return {
+            id: movie.id,
+            release_date: movie.release_date,
+            title: movie.title,
+            poster_path: movie.poster_path
+        }
+    })
+    return catalog
 }
